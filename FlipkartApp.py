2a3bbcb1b7e6b3a1eecd6108f2888e329c282399
Apoorv Bhiwandkar <42290080+zeapoorv@users.noreply.github.com>
2023-06-07 12:05:19 +0530

    Update FlipkartApp.py

--- FlipkartApp.py
+++ FlipkartApp.py
@@ -9,7 +9,7 @@ from streamlit import session_state as state
 page_bg = """
 <style>
 [data-testid="stAppViewContainer"]{
-background-image: url("https://cdn.pixabay.com/photo/2016/12/26/13/15/backgrounds-1932038_1280.jpg");
+background-image: url("https://images.pexels.com/photos/628241/pexels-photo-628241.jpeg");
 background-size: cover;
 }
 </style>
