bb47e38851c9691892f9824a0e5d6a36bd511089
Apoorv Bhiwandkar <42290080+zeapoorv@users.noreply.github.com>
2023-06-07 11:33:57 +0530

    Update FlipkartApp.py

--- FlipkartApp.py
+++ FlipkartApp.py
@@ -6,12 +6,21 @@ from urllib.parse import urlparse, parse_qs
 import streamlit as st
 from streamlit import session_state as state
 
+page_bg = """
+<style>
+[data-testid="stAppViewContainer"]{
+background-color: #e5e5f7;
+opacityL 0.2;
+filter: blur(20px);
+}
+</style>
+"""
 
 st.markdown("""
 <h1 style="display: flex; align-tems: center; justify-content: center; background-color: #F3F3DD; color: #047bd5; text-align: center; padding: 20px;">
 <img src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-inventory-management-system-zap-inventory-1.png" style="width: 40px; height:40px; margin-right:10px;"> Flipkart Scraper
 </h1>
-""", unsafe_allow_html=True)
+""", page_bg, unsafe_allow_html=True)
 
 # Initialize session state
 if 'urls' not in state:
