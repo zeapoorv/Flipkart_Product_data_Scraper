7136ab8019edacbc083f327dd17360d3ea0f01f8
Apoorv Bhiwandkar <42290080+zeapoorv@users.noreply.github.com>
2023-06-07 11:14:04 +0530

    Update FlipkartApp.py

--- FlipkartApp.py
+++ FlipkartApp.py
@@ -16,8 +16,8 @@ st.markdown("""
          }
 </style>
     
-<h1 style="display: flex; align-tems: center; justify-content: center; background-color: #F3F3DD; color: #047bd5; text-align: center; padding: 50px;">
-<img src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-inventory-management-system-zap-inventory-1.png" style="width: 60px; height:60px; margin-right:10px;"> Flipkart Scraper
+<h1 style="display: flex; align-tems: center; justify-content: center; background-color: #F3F3DD; color: #047bd5; text-align: center; padding: 20px;">
+<img src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-inventory-management-system-zap-inventory-1.png" style="width: 40px; height:40px; margin-right:10px;"> Flipkart Scraper
 </h1>
 """, unsafe_allow_html=True)
 
